18694ce88c1a4ee7e4c5bc00c2ed312799999cf4
Mykhailo Mokhoruk <michael119nr@gmail.com>
2018-10-29 13:23:08 +0200

    FindPic

--- FindPic/src/index.js
+++ FindPic/src/index.js
@@ -1,9 +1,9 @@
 import "./normalize.css";
 import "./styles.css";
-import imgHbs from "./templates/img.hbs";
-import imgFavHbs from "./templates/img-fav.hbs";
 import { apiImages } from "./services/api";
 import { LOCALSTORAGE } from "./services/storage";
+import imgHbs from "./templates/img.hbs";
+import imgFavHbs from "./templates/img-fav.hbs";
 import logo from "./img/logo.png";
 
 const header = document.querySelector(".header");
@@ -13,30 +13,28 @@ const favoritesBtn = document.querySelector(".btn-fav");
 const gallery = document.querySelector(".gallery");
 const cards = document.querySelector("#cards");
 const slider = document.querySelector(".js-slider");
-const btnMore = document.querySelector(".js-button");
-const h1 = document.querySelector(".text-fav");
+const btnMore = document.querySelector(".js-button-more");
+const textFav = document.querySelector(".text-fav");
 const spinners = document.querySelector(".spinners");
 
 const link = document.createElement("a");
 link.classList.add("link-logo");
 link.setAttribute("href", "./index.html");
 const img = document.createElement("img");
-img.classList.add("logo");
-img.alt = "FindPic";
 img.setAttribute("src", logo);
+img.alt = "FindPic";
 link.appendChild(img);
 header.insertBefore(link, form);
 
-let id;
+let idx;
 let markup;
 let page = 1;
 let arrPhotos = [];
-
 let arrFav = LOCALSTORAGE.get("arr-fav");
-if (arrFav === null) arrFav = [];
+if (!arrFav) arrFav = [];
 
-let arrFavorites = LOCALSTORAGE.get("arr-cards");
-if (arrFavorites === null) arrFavorites = [];
+let arrFavorites = LOCALSTORAGE.get("arr-fav-img");
+if (!arrFavorites) arrFavorites = [];
 
 form.addEventListener("submit", handleForm);
 
@@ -48,11 +46,13 @@ function handleForm(evt) {
   }
 
   spinner();
+  arrPhotos.length = 0;
+  page = 1;
   header.classList.add("shift");
   gallery.classList.add("bias");
   input.value = input.value.substr(0).toLowerCase();
-  h1.hidden = true;
-  resetImage(cards);
+  textFav.hidden = true;
+  reset(cards);
   handleRequest();
 }
 
@@ -60,17 +60,11 @@ function handleRequest() {
   apiImages(input.value, page).then(photos => {
     btnMore.hidden = true;
     if (photos === undefined || photos.length === 0) {
-      page = 1;
       spinner();
       return;
     }
 
-    if (page === 1) arrPhotos.length = 0;
-    if (photos.length >= 12) {
-      btnMore.hidden = false;
-    } else {
-      page = 1;
-    }
+    if (photos.length === 12) btnMore.hidden = false;
     spinner();
     createListImage(photos);
   });
@@ -78,7 +72,7 @@ function handleRequest() {
 
 function createListImage(arr) {
   markup = arr.reduce((acc, obj) => {
-    obj.id = arrPhotos.length;
+    obj.idx = arrPhotos.length;
     arrPhotos.push(obj);
     return acc + imgHbs({ obj });
   }, "");
@@ -97,18 +91,18 @@ function handleMoreImages() {
   handleRequest();
 }
 
-favoritesBtn.addEventListener("click", handleFavorites);
+favoritesBtn.addEventListener("click", handleFavPhoto);
 
-function handleFavorites() {
+function handleFavPhoto() {
   header.classList.add("shift");
-  h1.hidden = false;
-  resetImage(cards);
+  textFav.hidden = false;
+  reset(cards);
   arrPhotos.length = 0;
   page = 1;
   btnMore.hidden = true;
 
-  arrFavorites = LOCALSTORAGE.get("arr-cards");
-  arrFavorites !== null ? createListFavorites() : (arrFavorites = []);
+  arrFavorites = LOCALSTORAGE.get("arr-fav-img");
+  arrFavorites ? createListFavorites() : (arrFavorites = []);
 }
 
 function createListFavorites() {
@@ -127,14 +121,11 @@ function handleCards(evt) {
     return;
   if (evt.target.nodeName === "BUTTON") {
     evt.bool = true;
-    removeFavImg(evt);
-    return;
-  }
-
-  if (h1.hidden) {
-    someElm(evt, arrPhotos);
+    removeFavImg(evt, evt.target.id);
+  } else if (textFav.hidden) {
+    someElm(arrPhotos, evt);
   } else {
-    someElm(evt, arrFavorites);
+    someElm(arrFavorites, evt);
   }
 }
 
@@ -144,13 +135,12 @@ function handleSlideBtn(evt) {
   if (evt.target.nodeName !== "BUTTON") return;
 
   if (
-    evt.target.className === "btn-prev" ||
-    evt.target.className === "btn-next" ||
-    evt.target.className === "btn-close"
+    evt.target.className !== "btn-favorites" &&
+    evt.target.className !== "btn-favorites toggle"
   ) {
     slider.classList.remove("slider");
     favoritesBtn.classList.remove("z-index");
-    resetImage(slider);
+    reset(slider);
   }
 
   if (evt.target.className === "btn-close") return;
@@ -159,77 +149,66 @@ function handleSlideBtn(evt) {
     evt.target.className === "btn-prev" ||
     evt.target.className === "btn-next"
   ) {
-    if (h1.hidden) {
-      someElm(evt, arrPhotos);
+    if (textFav.hidden) {
+      someElm(arrPhotos, evt);
     } else {
-      someElm(evt, arrFavorites);
+      someElm(arrFavorites, evt);
     }
-    return;
-  }
-
-  if (
-    evt.target.className === "btn-favorites" ||
-    evt.target.className === "btn-favorites toggle"
-  ) {
+  } else {
     evt.target.classList.toggle("toggle");
+    let largeImg = document.querySelector(".img-web");
 
-    let favFoto = findElmFav(arrFavorites);
-    if (favFoto) {
-      id = evt.target.id;
-      evt.target.id = favFoto.id;
+    if (findElm(arrFavorites, largeImg.id)) {
       evt.bool = true;
-      removeFavImg(evt);
-      evt.target.id = id;
-    } else if (!h1.hidden) {
+      removeFavImg(evt, largeImg.id);
+    } else if (!textFav.hidden) {
       evt.bool = false;
-      let elm = findElmFav(arrFav);
+      let elm = findElm(arrFav, largeImg.id);
       arrFavorites.push(elm);
       removeFavImg(evt);
     } else {
-      let photo = findElmFav(arrPhotos);
+      let photo = findElm(arrPhotos, largeImg.id);
       handleArrPhoto(photo);
     }
   }
 }
 
-function findElmFav(arr) {
-  let largeImg = document.querySelector(".img-web");
-  return arr.find(
-    obj => obj.webformatURL === largeImg.src || obj.largeImageURL === largeImg.src
-  );
+function findElm(arr, id) {
+  return arr.find(obj => obj.id === Number(id));
 }
 
-function removeFavImg(evt) {
+function removeFavImg(evt, id) {
   if (evt.bool) {
-    arrFavorites = arrFavorites.filter(obj => obj.id !== Number(evt.target.id));
+    arrFavorites = arrFavorites.filter(obj => obj.id !== Number(id));
   }
 
-  arrFavorites.forEach((obj, idx) => (obj.id = idx));
+  arrFavorites.forEach((obj, idx) => (obj.idx = idx));
   if (!evt.bool) {
     evt.target.id = arrFavorites.length - 1;
     evt.target.previousSibling.id = arrFavorites.length;
     evt.target.previousSibling.previousSibling.id = arrFavorites.length - 2;
   }
 
-  LOCALSTORAGE.set("arr-cards", arrFavorites);
+  LOCALSTORAGE.set("arr-fav-img", arrFavorites);
 
-  if (h1.hidden) return;
-  handleFavorites();
+  if (!textFav.hidden) {
+    handleFavPhoto();
+  }
 }
 
 function handleArrPhoto(photo) {
-  id = photo.id;
-  photo.id = arrFavorites.length;
+  idx = photo.idx;
+  photo.idx = arrFavorites.length;
   arrFavorites.push(photo);
-  if (!arrFav.some(obj => obj.previewURL === photo.previewURL)) {
+  if (!arrFav.some(obj => obj.id === photo.id)) {
     arrFav.push(photo);
     LOCALSTORAGE.set("arr-fav", arrFav);
   }
-  LOCALSTORAGE.set("arr-cards", arrFavorites);
-  photo.id = id;
+  LOCALSTORAGE.set("arr-fav-img", arrFavorites);
+  photo.idx = idx;
 }
 
-function resetImage(elm) {
+function reset(elm) {
   elm.innerHTML = "";
 }
 
@@ -237,9 +216,9 @@ function spinner() {
   spinners.classList.toggle("visible");
 }
 
-function someElm(evt, arr) {
+function someElm(arr, evt) {
   arr.some(
-    obj => (obj.id === Number(evt.target.id) ? handleLargeImg(obj, evt) : null)
+    obj => (obj.idx === Number(evt.target.id) ? handleLargeImg(obj, evt) : null)
   );
 }
 
@@ -250,30 +229,43 @@ function handleLargeImg(obj, evt) {
   slideBtn.classList.add("slide-btn");
   let btnPrev = document.createElement("button");
   btnPrev.classList.add("btn-prev");
-  btnPrev.id = obj.id - 1;
+  btnPrev.id = obj.idx - 1;
   let btnNext = document.createElement("button");
   btnNext.classList.add("btn-next");
-  btnNext.id = obj.id + 1;
+  btnNext.id = obj.idx + 1;
   let btnFavorites = document.createElement("button");
   btnFavorites.classList.add("btn-favorites");
-  arrFavorites = LOCALSTORAGE.get("arr-cards");
-  if (arrFavorites === null) arrFavorites = [];
-  if (arrFavorites.some(objFav => objFav.previewURL === obj.previewURL)) {
+  btnFavorites.id = obj.idx;
+  arrFavorites = LOCALSTORAGE.get("arr-fav-img");
+  if (!arrFavorites) arrFavorites = [];
+  if (arrFavorites.some(objFav => objFav.id === obj.id)) {
     btnFavorites.classList.add("toggle");
   }
-  btnFavorites.id = obj.id;
   let btnClose = document.createElement("button");
   btnClose.classList.add("btn-close");
   slideBtn.append(btnPrev, btnNext, btnFavorites, btnClose);
+  let div = document.createElement("div");
+  div.classList.add("img-web-back");
   let imgWeb = document.createElement("img");
   imgWeb.classList.add("img-web");
-  imgWeb.setAttribute("alt", "Large Img");
-  if (evt.clientX > 1000) {
+  imgWeb.id = obj.id;
+  imgWeb.setAttribute("alt", "Large Image");
+  if (evt.clientX > 975) {
+    if (obj.largeImageURL === "") {
+      let imageW = obj.webformatWidth * 2;
+      let imageH = obj.webformatHeight * 2;
+      obj.largeImageURL = `https://placehold.it/${imageW}x${imageH}/231f20/ffffff/`;
+    }
     imgWeb.setAttribute("src", obj.largeImageURL);
   } else {
+    if (obj.webformatURL === "")
+      obj.webformatURL = `https://placehold.it/${obj.webformatWidth}x${
+        obj.webformatHeight
+      }/231f20/ffffff/`;
     imgWeb.setAttribute("src", obj.webformatURL);
   }
-  slide.appendChild(imgWeb);
+  div.appendChild(imgWeb);
+  slide.appendChild(div);
   slide.appendChild(slideBtn);
   slider.appendChild(slide);
   slider.classList.add("slider");
--- FindPic/webpack.config.js
+++ FindPic/webpack.config.js
@@ -62,6 +62,6 @@ module.exports = {
     stats: "errors-only",
     clientLogLevel: "warning",
     compress: true,
-    port: 9092
+    port: 9098
   }
 };
